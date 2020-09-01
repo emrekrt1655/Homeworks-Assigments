72188510e0c313e3e5a10e8054ab0f69865efb90
Emre <emrekurtt1655@gmail.com>
2020-09-01 10:05:48 +0200

    the multiplication table

---
+++ Python/if_statements_loops/carpim_tablosu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 1'den 10'kadar olan sayılarla ekrana çarpım tablosu bastırmaya çalışın.
+# 
+
+# In[3]:
+
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(i,"x", j ,"=", i*j)
+
+
+# In[ ]:
+
+
+
+
