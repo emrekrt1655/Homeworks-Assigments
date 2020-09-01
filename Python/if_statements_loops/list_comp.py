05a52b0c49ee1b15f8ed640635a57ef5005f684d
Emre <emrekurtt1655@gmail.com>
2020-09-01 10:11:41 +0200

    list comprehension method

---
+++ Python/if_statements_loops/list_comp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Buradaki problemin çözümünü derslerimizde özellikle öğrenmedik. Burada mantık yürüterek ve list comprehension kullanarak 1'den 100'e kadar olan sayılardan sadece çift sayıları bir listeye atmayı yapmayı çalışın.
+
+# In[1]:
+
+
+my_list = list(range(1,101))
+my_new_list = [i for i in my_lis]
+
+
+# In[ ]:
+
+
+
+
